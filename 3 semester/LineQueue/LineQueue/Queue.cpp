767f4842b931ea49127366c169d9d36ebb14d13e
rjastrzemskis <83876094+rjastrzemskis@users.noreply.github.com>
2021-09-08 07:19:12 +0300

    Update Queue.cpp

--- 3 semester/LineQueue/LineQueue/Queue.cpp
+++ 3 semester/LineQueue/LineQueue/Queue.cpp
@@ -1,61 +1,123 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
 #include <conio.h>
-#include <vector>
 
 using namespace std;
 struct Queue
 {
 private:
     int counter = 0;
-    vector<int> array;
-    int length = 0;
+    int* array;
+    int length;
+
 
 public: Queue(int startLength)
 {
     length = startLength;
+    array = new int[length] {};
 }
-    void AddValue(int value)
-    {
-	if (array.size() < length)
-       {
-            array.push_back(value);
-            counter++;
-       }
-        if (array.size() == length)
-        {
-            array.push_back(value);
-            length *= 2;
-            counter++;
-        }
-    }
 
-    int RemoveValue()
-    {
-        int value = 0;
-        if (array.size() > 0)
-        {
-            array.erase(array.begin());
-            value = array.size();
-		
-            if (array.size() == length-1)
-                length /= 2;
-        }
-        if (array.empty())
-        {
-            cout << "Queue is empty , there is nothing to remove !" << endl;
-            value = INT_MIN;
-        }
-        return value;
-    }
+      void AddValue(int value)
+      {
+          int arraySize = 0;
+          for (int i = 0; i < length; i++)
+          {
+              if (array[i] != NULL)
+                  arraySize++;
+              else i = length;
+          }
 
-    void ToString()
-    {
-        for (int i = 0; i < array.size(); i++)
-            cout << array.at(i) << ' ';
+          if (arraySize < length)
+          {
+              for (int i = 0; i < length; i++)
+              {
+                  if (array[i] == NULL)
+                  {
+                      array[i] = value;
+                      i = length;
+                  }
+              }
+              counter++;
+          }
+          else
+          {
+              length = length * 2;
+              int* increasedArray = new int[length];;
+              for (int i = 0; i < length; i++)
+              {
+                  if (i < length / 2)
+                      increasedArray[i] = array[i];
+                  else
+                      increasedArray[i] = NULL;
+              }
+              delete[]array;
+              array = increasedArray;
+              increasedArray = NULL;
 
-        cout << endl;
-    }
+              for (int i = 0; i < length; i++)
+              {
+                  if (array[i] == NULL)
+                  {
+                      array[i] = value;
+                      i = length;
+                  }
+              }
+              counter++;
+          }
+      }
+
+      int RemoveValue()
+      {
+          int value = 0, arraySize = 0;
+          for (int i = 0; i < length; i++)
+          {
+              if (array[i] != NULL)
+                  arraySize++;
+              else i = length;
+          }
+
+          if (arraySize > 0)
+          {
+              value = array[0];
+              for (int i = 0; i < length; i++)
+                  if (i < arraySize - 1)
+                      array[i] = array[i + 1];
+                  else array[i] = NULL;
+
+              if (arraySize == length && arraySize < length)
+              {
+                  length = length / 2;
+                  int* decreasedArray = new int[length];
+                  for (int i = 0; i < length; i++)
+                      decreasedArray[i] = array[i];
+
+                  delete[]array;
+                  array = decreasedArray;
+                  decreasedArray = NULL;
+              }
+          }
+          else
+          {
+              cout << "Queue is empty , there is nothing to remove!" << endl;
+              value = INT_MIN;
+          }
+          return value;
+      }
+
+      void ToString()
+      {
+          int arraySize = 0;
+          for (int i = 0; i < length; i++)
+          {
+              if (array[i] != NULL)
+                  arraySize++;
+              else i = length;
+          }
+
+          for (int i = 0; i < arraySize; i++)
+              cout << array[i] << ' ';
+
+          cout << endl;
+      }
 };
 
 void main()
@@ -70,12 +132,12 @@ void main()
         cout << "2 To remove value from queue" << endl;
         cout << "3 To output queue on screen" << endl;
         cout << "0 To stop program" << endl;
-        cout << "Enter number from 0 to 3: " , cin >> userInput;
+        cout << "Enter number from 0 to 3: ", cin >> userInput;
 
         switch (userInput)
         {
         case 1:
-            cout << "Please enter value to add it to queue: " , cin >> x;
+            cout << "Please enter value to add it to queue: ", cin >> x;
             queue.AddValue(x);
             break;
         case 2:
