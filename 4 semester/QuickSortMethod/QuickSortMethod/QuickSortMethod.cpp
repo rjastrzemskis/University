818e6e2086dbbf770afc0fbacdce5545df791471
rjastrzemskis <r.jastrzemskis@gmail.com>
2022-03-06 18:45:31 +0200

    Add files via upload

---
+++ 4 semester/QuickSortMethod/QuickSortMethod/QuickSortMethod.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void quick_sort(int numberArray[], int low, int high);
+int main()
+{
+    //QuickSort method
+    int numberArray[] = { -2, 22, 2, 0, 44 };
+    int arrayLength = sizeof(numberArray) / sizeof(*numberArray);
+
+    cout << endl << "Before QuickSort method: ";
+    for (int i = 0; i < arrayLength; i++)
+    {
+        cout << numberArray[i] << " ";
+    }
+
+    quick_sort(numberArray, 0, arrayLength - 1);
+
+    cout << endl << "After QuickSort method: ";
+    for (int i = 0; i < arrayLength; i++)
+    {
+        cout << numberArray[i] << " ";
+    }
+}
+
+
+void quick_sort(int numberArray[], int low, int high) {
+    int temp, i, j, p = 0;
+
+    if (low < high) {
+        p = low;
+        i = low;
+        j = high;
+
+        while (i < j) {
+            while (numberArray[i] <= numberArray[p] && i < high)
+                i++;
+            while (numberArray[j] > numberArray[p])
+                j--;
+            if (i < j) {
+                temp = numberArray[i];
+                numberArray[i] = numberArray[j];
+                numberArray[j] = temp;
+            }
+        }
+
+        temp = numberArray[p];
+        numberArray[p] = numberArray[j];
+        numberArray[j] = temp;
+        quick_sort(numberArray, low, j - 1);
+        quick_sort(numberArray, j + 1, high);
+    }
+}
