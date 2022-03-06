b9e6d0a3128a7dc78a98d6ac36c738d03fa00f1a
rjastrzemskis <r.jastrzemskis@gmail.com>
2022-03-06 18:43:33 +0200

    Update ShellMethod.cpp
    typo

--- 4 semester/ShellMethod/ShellMethod/ShellMethod.cpp
+++ 4 semester/ShellMethod/ShellMethod/ShellMethod.cpp
@@ -4,36 +4,34 @@ using namespace std;
 
 int main()
 {
+    //Shell method
+    int numberArray[] = { -2, 22, 2, 0, 44 };
+    int arrayLength = sizeof(numberArray) / sizeof(*numberArray);
+    int temp = 0;
+    int i = 0;
+
+    cout << endl << "Before shell method: ";
+    for (int i = 0; i < arrayLength; i++)
     {
-        //Shell method
-        int numberArray[] = { -2, 22, 2, 0, 44 };
-        int arrayLength = sizeof(numberArray) / sizeof(*numberArray);
-        int temp = 0;
-        int i = 0;
+        cout << numberArray[i] << " ";
+    }
 
-        cout << endl << "Before shell method: ";
-        for (int i = 0; i < arrayLength; i++)
-        {
-            cout << numberArray[i] << " ";
-        }
-
-		for (int x = arrayLength / 2; x > 0; x/= 2)
+	for (int x = arrayLength / 2; x > 0; x/= 2)
+	{
+		for (int j = x; j < arrayLength; j += 1)
 		{
-			for (int j = x; j < arrayLength; j += 1)
+			temp = numberArray[j];
+			for (i = j; (i >= x) && (numberArray[i - x] > temp); i -= x)
 			{
-				temp = numberArray[j];
-				for (i = j; (i >= x) && (numberArray[i - x] > temp); i -= x)
-				{
-					numberArray[i] = numberArray[i - x];
-				}
-				numberArray[i] = temp;
+				numberArray[i] = numberArray[i - x];
 			}
+			numberArray[i] = temp;
 		}
+	}
 
-        cout << endl << "After shell method: ";
-        for (int i = 0; i < arrayLength; i++)
-        {
-            cout << numberArray[i] << " ";
-        }
+    cout << endl << "After shell method: ";
+    for (int i = 0; i < arrayLength; i++)
+    {
+        cout << numberArray[i] << " ";
     }
 }
