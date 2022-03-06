84423f72a9823add83bb22dbb4d6f04d18dfd7b1
rjastrzemskis <r.jastrzemskis@gmail.com>
2022-03-06 17:49:03 +0200

    Add files via upload

---
+++ 4 semester/ShellMethod/ShellMethod/ShellMethod.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    {
+        //Shell method
+        int numberArray[] = { -2, 22, 2, 0, 44 };
+        int arrayLength = sizeof(numberArray) / sizeof(*numberArray);
+        int temp = 0;
+        int i = 0;
+
+        cout << endl << "Before shell method: ";
+        for (int i = 0; i < arrayLength; i++)
+        {
+            cout << numberArray[i] << " ";
+        }
+
+		for (int x = arrayLength / 2; x > 0; x/= 2)
+		{
+			for (int j = x; j < arrayLength; j += 1)
+			{
+				temp = numberArray[j];
+				for (i = j; (i >= x) && (numberArray[i - x] > temp); i -= x)
+				{
+					numberArray[i] = numberArray[i - x];
+				}
+				numberArray[i] = temp;
+			}
+		}
+
+        cout << endl << "After shell method: ";
+        for (int i = 0; i < arrayLength; i++)
+        {
+            cout << numberArray[i] << " ";
+        }
+    }
+}
