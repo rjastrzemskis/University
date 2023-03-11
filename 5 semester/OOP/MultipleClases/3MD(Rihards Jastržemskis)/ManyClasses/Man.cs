9fd6827573cbfe9d3bf5676c86df3a43cabd6373
rjastrzemskis <r.jastrzemskis@gmail.com>
2023-03-11 22:07:16 +0200

    Add files via upload

---
+++ 5 semester/OOP/MultipleClases/3MD(Rihards Jastržemskis)/ManyClasses/Man.cs
@@ -0,0 +1,29 @@
+﻿namespace ManyClasses
+{
+    public class Man
+    {
+        private string name;
+        private string surname;
+
+        public Man(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+        }
+
+        public string FullName
+        {
+            get { return $"{name} {surname}"; }
+        }
+    }
+}
