84e602744d5dd92b8ed41fe8eca4012bb850a386
rjastrzemskis <r.jastrzemskis@gmail.com>
2023-03-11 22:06:37 +0200

    Add files via upload

---
+++ 5 semester/OOP/MultipleClases/3MD(Rihards Jastržemskis)/ManStudentIntern/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManStudentIntern
+{
+    class Student : Man
+    {
+        private string school;
+
+        public string School
+        {
+            set { school = value; }
+        }
+
+        public new void WriteInfo()
+        {
+            base.WriteInfo();
+            Console.WriteLine($"School: {school}");
+        }
+    }
+}
---
+++ 5 semester/OOP/MultipleClases/3MD(Rihards Jastržemskis)/RefrigeratorProduct/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RefrigeratorProduct
+{
+    class Product
+    {
+        private string productName;
+        private double weight;
+
+        public Product(string productName, double weight)
+        {
+            this.productName = productName;
+            this.weight = weight;
+        }
+        public void ReadInfo()
+        {
+            Console.WriteLine($"{productName} of {weight}kg");
+        }
+    }
+}
